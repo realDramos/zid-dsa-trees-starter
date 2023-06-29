ca75aefb53056ed38e9b88bf5de210f30dd840c6
realDramos <dannyramos1112@gmail.com>
2023-06-29 01:50:23 -0700

    full bst created

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,103 @@
 class BinarySearchTree {
-  // your code here
+  constructor(key = null, value = null, parent = null){
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value){
+    if(this.key == null){
+      this.key = key;
+      this.value = value;
+    }else if(key<this.key){
+      if(this.left == null){
+        this.left = new BinarySearchTree(key, value, this);
+      }else{
+        this.left.insert(key,value);
+      }
+    }else{
+      if(this.right == null){
+        this.right = new BinarySearchTree(key, value, this);
+      }else{
+        this.right.insert(key,value);
+      }
+    }
+  }
+
+  find(key){
+    if(this.key == key){
+      return this.value;
+    }else if(key < this.key && this.left){
+      return this.left.find(key);
+    }else if(key> this.key && this.right){
+      return this.right.find(key);
+    }else{
+      throw new Error('key not found');
+    }
+  }
+
+  remove(key){
+    if(this.key == key){
+      if(this.left && this.right){
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }else if(this.left){
+        this._replaceWith(this.left);
+      }else if(this.right){
+        this._replaceWith(this.right);
+      }else{
+        this._replaceWith(null);
+      }
+    }else if(key<this.key && this.left){
+      this.left.remove(key);
+    }else if(key> this.key && this.right){
+      this.right.remove(key);
+    }else{
+      throw new Error('Key not found');
+    }
+  }
+
+  _replaceWith(node){
+   if(this.parent){
+    if(this == this.parent.left){
+      this.parent.left = node;
+    }else if(this == this.parent.right){
+      this.parent.right = node
+    }else if(node){
+      node.parent = node;
+    }
+   }else{
+    if(node){
+      this.key = node.key;
+      this.value = node.value;
+      this.left = node.left;
+      this.right = node.right;
+    }else{
+      this.key = null;
+      this.value = null;
+      this.right = null;
+      this.left = null;
+    }
+   }
+  }
+  _findMin(){
+    if(!this.left){
+      return this;
+    }
+    return this.left._findMin();
+  }
+ 
 }
+
+const bst = new BinarySearchTree(5);
+bst.insert(2);
+bst.insert(19);
+bst.insert(15);
+bst.insert(28);
+bst.insert(18);
+
+console.log(bst);
